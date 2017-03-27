877ca19b097f669bd4ae4a31647e63069e8c193b
Aga <a.kolia.grudzien@gmail.com>
2017-03-27 22:29:04 +0200

    Formatted code.

--- METHODS/multiple_conditions_with_II_ operator.rb
+++ METHODS/multiple_conditions_with_II_ operator.rb
@@ -6,6 +6,7 @@ mood = "Happy"
 if budget > price || mood == "Happy"
   puts "I'm going to buy that"
 end
+
 # II means or and in the above-mentioned example or the budget must be
 #grater that price or the mood must equal "Happy"
 if budget > price || price < 8 || mood == "Happy"
